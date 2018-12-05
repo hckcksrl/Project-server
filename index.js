4a19a6ab1c2e53d8a7d5fb12d7381e0b16edbf60
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-05 15:11:34 +0900

    App Start

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, PubSub } from "graphql-yoga";
 import resolvers from "./resolvers";
+import * as typeorm from "typeorm";
 
 const server = new GraphQLServer({
   typeDefs: "schema.graphql",
   resolvers
 });
 
-server.start(() => console.log("server login"));
+typeorm
+  .createConnection({
+    type: "postgres",
+    database: "project",
+    synchronize: true,
+    logging: false,
+    entities: [
+      require("./Entity/UserSchema"),
+      require("./Entity/SubProjectSchema"),
+      require("./Entity/ProjectSchema"),
+      require("./Entity/DetailProjectSchema")
+    ],
+    host: "localhost",
+    port: 5432,
+    username: "hckcksrl",
+    password: "1234"
+  })
+  .then(() => {
+    server.start(() => console.log("Start"));
+  });
