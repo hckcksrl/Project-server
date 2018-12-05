376e374e19890865b40ae3f00f1dedc428c7fe71
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-05 16:05:23 +0900

    DetailList Modeling

---
+++ Model/DetailList.js
@@ -0,0 +1,10 @@
+class DetailList {
+  constructor(id, DetailName, SubProjectId) {
+    (this.id = id),
+      (this.DetailName = DetailName),
+      (this.SubProjectId = SubProjectId);
+  }
+}
+module.exports = {
+  DetailList: DetailList
+};
