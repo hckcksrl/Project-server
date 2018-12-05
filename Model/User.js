aa1d0e066cb2686af2e47be46932d6db6850875c
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-05 15:11:55 +0900

    User Modeling

---
+++ Model/User.js
@@ -0,0 +1,11 @@
+class User {
+  constructor(id, email, password, project) {
+    (this.id = id),
+      (this.email = email),
+      (this.password = password),
+      (this.project = project);
+  }
+}
+module.export = {
+  User: User
+};
