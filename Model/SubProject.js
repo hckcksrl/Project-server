08998aaced498a9a6c6ce403dd988e39331d2ab9
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-05 15:12:04 +0900

    SubProject Modeiling

---
+++ Model/SubProject.js
@@ -0,0 +1,11 @@
+class SubProject {
+  constructor(id, SubProjectName, DetailList, ProjectId) {
+    (this.id = id),
+      (this.SubProjectName = SubProjectName),
+      (this.DetailList = DetailList),
+      (this.ProjectId = ProjectId);
+  }
+}
+module.export = {
+  SubProject: SubProject
+};
