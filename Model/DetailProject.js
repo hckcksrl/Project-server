00870a5cfc69aba252d7a4727c1be4f237d6ed8c
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-05 15:12:24 +0900

    DetailProject Modeling

---
+++ Model/DetailProject.js
@@ -0,0 +1,10 @@
+class DetailProject {
+  constructor(id, DetailName, SubProjectId) {
+    (this.id = id),
+      (this.DetailName = DetailName),
+      (this.SubProjectId = SubProjectId);
+  }
+}
+module.exports = {
+  DetailProject: DetailProject
+};
