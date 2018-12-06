d0cd69ebc702e890737b1919558e68b0b2c83bc1
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-06 20:03:43 +0900

    Projects  Table Interface

--- Entity/ProjectSchema.js
+++ Entity/ProjectSchema.js
@@ -1,33 +1,55 @@
-import { EntitySchema } from "typeorm";
-import { Projects } from "../Model/Project";
-import { Users } from "../Model/User";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  ManyToOne
+} from "typeorm";
+import SubProjects from "./SubProjectSchema";
+import Users from "./UserSchema";
 
-export const Project = new EntitySchema({
-  name: "Projects",
-  target: Projects,
-  columns: {
-    id: {
-      primary: true,
-      type: Number,
-      generated: true
-    },
-    projectName: {
-      type: String,
-      nullable: false
-    }
-  },
-  relations: {
-    subProject: {
-      type: "on-to-many",
-      target: "SubProjects",
-      joinTable: true,
-      cascade: true
-    },
-    userId: {
-      type: "many-to-one",
-      target: "Users",
-      joinTable: true,
-      cascade: true
-    }
-  }
-});
+@Entity()
+class Projects extends BaseEntity {
+  @PrimaryGeneratedColumn() id = undefined;
+
+  @Column({ type: "text", nullable: false })
+  projectName = "";
+
+  @OneToMany(type => SubProjects, sub_projects => sub_projects.SubProjectName)
+  subProject = SubProjects;
+
+  @ManyToOne(type => Users, users => users.id)
+  userId = undefined;
+}
+export default Projects;
+
+// export const Project = new EntitySchema({
+//   name: "Projects",
+//   target: Projects,
+//   columns: {
+//     id: {
+//       primary: true,
+//       type: Number,
+//       generated: true
+//     },
+//     projectName: {
+//       type: String,
+//       nullable: false
+//     }
+//   },
+//   relations: {
+//     subProject: {
+//       type: "one-to-many",
+//       target: "SubProjects",
+//       joinTable: true,
+//       cascade: true
+//     },
+//     userId: {
+//       type: "many-to-one",
+//       target: "Users",
+//       joinTable: true,
+//       cascade: true
+//     }
+//   }
+// });
