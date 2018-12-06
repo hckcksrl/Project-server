3bedb07bdef29598f2c11dc28d17d970f4cc5451
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-06 20:48:53 +0900

    GetSubProject Resolver

---
+++ API/SubProject/GetSubProject/resolvers.js
@@ -0,0 +1,27 @@
+import SubProjects from "../../../Entity/SubProjectSchema";
+
+const resolvers = {
+  Query: {
+    GetSubProject: async (_, args) => {
+      const subProject = await SubProjects.findOne({
+        where: {
+          id: args.id
+        }
+      });
+      if (subProject != null) {
+        return {
+          result: true,
+          error: null,
+          subproject: subProject
+        };
+      } else {
+        return {
+          result: false,
+          error: "not found",
+          subproject: null
+        };
+      }
+    }
+  }
+};
+export default resolvers;
