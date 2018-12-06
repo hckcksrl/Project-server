ace72e532100e966c6d552f727f0849ba60b49a7
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-06 20:03:49 +0900

    SubProject  Table Interface

--- Entity/SubProjectSchema.js
+++ Entity/SubProjectSchema.js
@@ -1,33 +1,55 @@
-import { EntitySchema } from "typeorm";
-import { SubProjects } from "../Model/SubProject";
-import { Projects } from "../Model/Project";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  ManyToOne
+} from "typeorm";
+import DetailList from "./DetailListSchema";
+import Projects from "./ProjectSchema";
 
-export const SubProject = new EntitySchema({
-  name: "SubProjects",
-  target: SubProjects,
-  columns: {
-    id: {
-      primary: true,
-      type: Number,
-      generated: true
-    },
-    SubProjectName: {
-      type: String,
-      nullable: false
-    }
-  },
-  relations: {
-    DetailList: {
-      type: "on-to-many",
-      target: "DetailList",
-      joinTable: true,
-      cascade: true
-    },
-    ProjectId: {
-      type: "many-to-one",
-      target: "Projects",
-      joinTable: true,
-      cascade: true
-    }
-  }
-});
+@Entity()
+class SubProjects extends BaseEntity {
+  @PrimaryGeneratedColumn() id = undefined;
+
+  @Column({ type: "text", nullable: false })
+  SubProjectName = "";
+
+  @ManyToOne(type => Projects, projects => projects.id)
+  ProjectId = undefined;
+
+  @OneToMany(type => DetailList, detail_list => detail_list.DetailName)
+  DetailList = DetailList;
+}
+export default SubProjects;
+
+// export const SubProject = new EntitySchema({
+//   name: "SubProjects",
+//   target: SubProjects,
+//   columns: {
+//     id: {
+//       primary: true,
+//       type: Number,
+//       generated: true
+//     },
+//     SubProjectName: {
+//       type: String,
+//       nullable: false
+//     }
+//   },
+//   relations: {
+//     DetailList: {
+//       type: "on-to-many",
+//       target: "DetailList",
+//       joinTable: true,
+//       cascade: true
+//     },
+//     ProjectId: {
+//       type: "many-to-one",
+//       target: "Projects",
+//       joinTable: true,
+//       cascade: true
+//     }
+//   }
+// });
