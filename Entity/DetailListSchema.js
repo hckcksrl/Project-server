bb147abe1afab0e2a8b55b7f1fc7be831cbbb6a9
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-06 20:03:38 +0900

    DetailList Table Interface

--- Entity/DetailListSchema.js
+++ Entity/DetailListSchema.js
@@ -1,23 +1,41 @@
-import { EntitySchema } from "typeorm";
-import { DetailList } from "../Model/DetailList";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne
+} from "typeorm";
+import SubProjects from "./SubProjectSchema";
 
-export const Detail = new EntitySchema({
-  name: "DetailList",
-  target: DetailList,
-  columns: {
-    id: {
-      primary: true,
-      type: Number,
-      generated: true
-    },
-    DetailName: {
-      type: String
-    }
-  },
-  relations: {
-    SubProjectId: {
-      type: "many-to-one",
-      target: "SubProjects"
-    }
-  }
-});
+@Entity()
+class DetailList extends BaseEntity {
+  @PrimaryGeneratedColumn() id = undefined;
+
+  @Column({ type: "text", nullable: false })
+  DetailName = "";
+
+  @ManyToOne(type => SubProjects, sub_projects => sub_projects.id)
+  SubProjectId = undefined;
+}
+export default DetailList;
+
+// export const Detail = new EntitySchema({
+//   name: "DetailList",
+//   target: DetailList,
+//   columns: {
+//     id: {
+//       primary: true,
+//       type: Number,
+//       generated: true
+//     },
+//     DetailName: {
+//       type: String
+//     }
+//   },
+//   relations: {
+//     SubProjectId: {
+//       type: "many-to-one",
+//       target: "SubProjects"
+//     }
+//   }
+// });
