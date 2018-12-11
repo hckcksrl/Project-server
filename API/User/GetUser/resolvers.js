c3e7d3d466e8d39508986f0f2191fe353efa79e3
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-11 19:09:43 +0900

    GetUser And Nested Resolver

--- API/User/GetUser/resolvers.js
+++ API/User/GetUser/resolvers.js
@@ -1,14 +1,38 @@
-import Connection from "../../../database";
 import Projects from "../../../Entity/ProjectSchema";
-
+import Users from "../../../Entity/UserSchema";
+import SubProjects from "../../../Entity/SubProjectSchema";
+import DetailList from "../../../Entity/DetailListSchema";
 const resolvers = {
+  User: {
+    project: async ({ id }, args) => {
+      return await Projects.find({
+        where: {
+          user_: id
+        }
+      });
+    }
+  },
+  Project: {
+    subproject: async ({ id }, args) => {
+      return await SubProjects.find({
+        where: {
+          project_: id
+        }
+      });
+    }
+  },
+  SubProject: {
+    detaillist: async ({ id }, args) => {
+      return await DetailList.find({
+        where: {
+          subproject_: id
+        }
+      });
+    }
+  },
   Query: {
     GetUser: async (_, args) => {
-      // const users = await getRepository(Users)
-      //   .createQueryBuilder("users")
-      //   .where("id = :id", { id: args.id })
-      //   .execute();
-      const users = await Connection.model("users").findOne({
+      const users = await Users.findOne({
         where: {
           id: args.id
         }
