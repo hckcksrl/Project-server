a103f3e30e711c1a3e0e16e8429876d4dfba13bb
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-11 19:08:00 +0900

    Edit Project Resolver

--- API/Projects/EditProject/resolvers.js
+++ API/Projects/EditProject/resolvers.js
@@ -1,9 +1,28 @@
 import Projects from "../../../Entity/ProjectSchema";
 import { astFromValue } from "graphql";
+import { getRepository } from "typeorm";
 
 const resolvers = {
   Mutation: {
-    EditProject: async (_, args) => {}
+    EditProject: async (_, args) => {
+      try {
+        await getRepository(Projects)
+          .createQueryBuilder()
+          .update()
+          .set({ projectname: args.projectname })
+          .where({ id: args.id })
+          .execute();
+        return {
+          result: true,
+          error: null
+        };
+      } catch (error) {
+        return {
+          result: false,
+          error: "Edit False"
+        };
+      }
+    }
   }
 };
 export default resolvers;
