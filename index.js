c729a77def0d837334a5f4ab9e774f3d67c11a61
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-11 19:06:23 +0900

    App Start

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 import { GraphQLServer } from "graphql-yoga";
-// import resolvers from "./resolvers";
-// import { createConnection } from "typeorm";
-// import ConnectionOptions from "./database";
+import { createConnection } from "typeorm";
+import ConnectionOptions from "./database";
 import schema from "./schema";
-import Relation from "./Entity/Relation";
-import Connection from "./database";
 
 const server = new GraphQLServer({
   schema
@@ -18,12 +15,8 @@ const appOption = {
 };
 
 const result = () => console.log("Start");
-Relation();
-server.start(appOption, result);
-
-// Relation();
-// createConnection(ConnectionOptions)
-//   .then(() => {
-// server.start(appOption, result);
-// })
-// .catch(error => console.log(error));
+createConnection(ConnectionOptions)
+  .then(() => {
+    server.start(appOption, result);
+  })
+  .catch(error => console.log(error));
