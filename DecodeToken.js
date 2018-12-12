6c7e8421c86f439e3b80ed4c225ffdc6b377bb9f
Hong Chan Ki <36074394+hckcksrl@users.noreply.github.com>
2018-12-12 18:10:25 +0900

    Decode Json Web Token

---
+++ DecodeToken.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import Users from "../Entity/UserSchema";
+dotenv.config({ path: "../bin/.env" });
+
+const DecodeJwt = async token => {
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET || "");
+    const { email } = decode;
+    console.log(email);
+    const user = await Users.findOne({
+      where: {
+        email: email
+      }
+    });
+    return user;
+  } catch (error) {
+    return error;
+  }
+};
+
+export default DecodeJwt;
